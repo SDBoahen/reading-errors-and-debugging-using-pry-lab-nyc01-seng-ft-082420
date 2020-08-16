86ff685d1cc9dc1ca0b75fa7ceafef74ba3a3046
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-16 04:04:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,27 @@
-# don't forget to add: require 'pry'
+# don't forget to add: 
 
-def start_game(player1, player2)
+require 'pry'
+
+
+
+
+def 
+  
+  start_game(player1, player2)
+  
+  
   "Hello #{player1} & #{playar2}"
+
 end
 
-def play_game(player2)
+
+
+
+def 
+  
+  play_game(player2)
+  
+  
   "#{player1} is better than #{player2}"
+
 end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,42 @@
-# don't forget to add: require 'pry'
+# don't forget to add: 
 
-def generate_star_date
+require 'pry'
+
+
+
+
+def 
+  
+  generate_star_date
+  
   (rand(100000) + 400000) / 10.0
+
 end
 
 
-def state_log(star_date)
+
+
+def 
+  
+  state_log(star_date)
+  
   "Captain's Log, star date #{star_date}."
+
 end
 
-def engage
+
+
+
+def 
+  
+  engage
+  
+  
   puts state_log(date)
+  
   date = generate_star_date
+
 end
+
+
+
