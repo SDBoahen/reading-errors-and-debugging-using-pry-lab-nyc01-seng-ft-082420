65f4675914b537f034033f76a8cf9baddf3930ce
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-16 04:29:42 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,24 +1,74 @@
-# don't forget to add: require 'pry'
+# don't forget to add: 
+require 'pry'
+
+
+
 
 def get_user_input
+  
   gets.chomp
+
 end
 
+
 def prompt_user
+  
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
+
 end
 
-def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
-  end
+
+
+
+def 
+  
+  selection(num)
+  
+  # ***
+  #binding.pry
+  # ~ shouldn't we wanna check before each case ? ~ emhmoo -
+  
+    case num
+    
+    when 1 
+         "YUM YUM MUNCH MUNCH MUNCH"
+    when 2
+         "HAM HAM HAM IN MY TUMMY"
+     
+    end
+  
+  # if num = 1
+    
+  #   #binding.pry
+    
+  #   "YUM YUM MUNCH MUNCH MUNCH"
+     
+  # elsif num = 2
+  
+  #   #binding.pray 
+      
+  #   "HAM HAM HAM IN MY TUMMY"
+     
+  # end
+     
+
 end
 
-def runner
+
+
+
+def 
+  
+  runner
+  
+  
   prompt_user
+  
   selection(get_user_input)
+  
 end
+
+
+
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,56 @@
-# don't forget to add: require 'pry'
+# don't forget to add: 
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
+require 'pry'
+
+
+
+
+def 
+  
+  
+  snake_it_up(string)
+  
+  
+    count   = 0
+    # {
+    x_times = 10
+    # }
+    
+    s_tack = ""
+  
+  
+    if string[0] == "s"
+    
+     #FuntionBased/Scalable/(FlexFLuid)
+     while (count < x_times)
+     
+           s_tack = s_tack + "s"
+           
+           count +=1
+     end
+     
+     s_tack + string
+     
+     
+     #    H||(
+     
+     #Funcitonal/Basic/(Hard Coded)
+     #"ssssssssss" + string
+     
+     
+     # \X/
+     #
+     #10 * "s" + string
+     #
+     #  emoobh :: (ERROR)(Type Error)
+     #  ** Can't LITERALLY Multiply the "s" 10times
+     # // emswet // 
+    
+    #    )||H
+    
+    else string
+  
+    end
+
+
 end
